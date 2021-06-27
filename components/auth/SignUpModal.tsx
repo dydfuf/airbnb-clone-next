53db00ca37509e59ababb12dd4f0ba30ae8e1596
dydfuf <88dydfuf@naver.com>
2021-06-27 17:32:47 +0900

    [회원가입 및 로그인] 로그인 모달 디자인 구현

---
+++ components/auth/SignUpModal.tsx
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import palette from "../../styles/palette";
+import CloseXIcon from "../../public/static/svg/modal/CloseXIcon.svg";
+import MailIcon from "../../public/static/svg/auth/MailIcon.svg";
+import PersonIcon from "../../public/static/svg/auth/PersonIcon.svg";
+import OpenedEyeIcon from "../../public/static/svg/auth/OpenedEyeIcon.svg";
+import ClosedEyeIcon from "../../public/static/svg/auth/ClosedEyeIcon.svg";
+import Input from "../common/Input";
+import Selector from "../common/Selector";
+import { dayList, monthList, yearList } from "../../lib/staticData";
+import Button from "../common/Button";
+
+const Container = styled.form`
+  width: 568px;
+  padding: 32px;
+  background-color: white;
+  z-index: 11;
+
+  .mordal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+  }
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
+  .sign-up-birthdat-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
+  }
+`;
+
+const SignUpModal: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [password, setPassword] = useState("");
+
+  //* 비밀번호 가리기
+  const [hidePassword, setHidePassword] = useState(true);
+
+  //* 셀렉터 값
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
+
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangeLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastname(e.target.value);
+  };
+
+  const onChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstname(e.target.value);
+  };
+
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onChangeBirthMonth = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(e.target.value);
+  };
+
+  const onChangeBirthDay = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(e.target.value);
+  };
+
+  const onChangeBirthYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(e.target.value);
+  };
+
+  return (
+    <Container>
+      <CloseXIcon className="mordal-close-x-icon" />
+      <div className="input-wrapper">
+        <Input
+          placeholder="이메일 주소"
+          type="email"
+          icon={<MailIcon />}
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="이름(예: 길동)"
+          icon={<PersonIcon />}
+          value={lastname}
+          onChange={onChangeLastName}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="성(예: 홍)"
+          icon={<PersonIcon />}
+          value={firstname}
+          onChange={onChangeFirstName}
+        />
+      </div>
+      <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input
+          placeholder="비밀번호 설정하기"
+          type={hidePassword ? "password" : "text"}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
+      <p className="sign-up-birthdat-label"> 생일 </p>
+      <p className="sign-up-modal-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
+        에어비엔비 이용자에게 공개되지 않습니다.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector
+            options={monthList}
+            defaultValue="월"
+            disabledOptions={["월"]}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector
+            options={dayList}
+            defaultValue="일"
+            disabledOptions={["일"]}
+            onChange={onChangeBirthDay}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector
+            options={yearList}
+            defaultValue="년"
+            disabledOptions={["년"]}
+            onChange={onChangeBirthYear}
+          />
+        </div>
+      </div>
+      <div className="sign-up-modal-submit-button-wrapper">
+        <Button type="submit">가입하기</Button>
+      </div>
+    </Container>
+  );
+};
+
+export default SignUpModal;
---
+++ hooks/useModal.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  .modal-backgroud {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.75);
+    z-index: 10;
+  }
+`;
+
+const useModal = () => {
+  const [modalOpened, setmodalOpened] = useState(false);
+
+  const openModal = () => {
+    setmodalOpened(true);
+  };
+
+  const closeModal = () => {
+    setmodalOpened(false);
+  };
+
+  interface IProps {
+    children: React.ReactNode;
+  }
+
+  const ModalPortal: React.FC<IProps> = ({ children }) => {
+    const ref = useRef<Element | null>();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+      setMounted(true);
+      if (document) {
+        const dom = document.querySelector("#root-modal");
+        ref.current = dom;
+      }
+    }, []);
+
+    if (ref.current && mounted) {
+      return createPortal(
+        <Container>
+          <div
+            className="modal-backgroud"
+            role="presentation"
+            onClick={closeModal}
+          />
+          {children}
+        </Container>,
+        ref.current
+      );
+    }
+    return null;
+  };
+  return {
+    openModal,
+    closeModal,
+    ModalPortal,
+  };
+};
+
+export default useModal;
