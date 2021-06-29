ac573ca0099e4ccda326f885cf1342a8e1028c3b
dydfuf <88dydfuf@naver.com>
2021-06-29 20:35:53 +0900

    [숙소등록하기] 리덕스 설정 및 1단계

---
+++ pages/room/register/building.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { NextPage } from "next";
+import RegisterRoombuilding from "../../../components/room/register/RegisterRoomBuilding";
+
+const building: NextPage = () => {
+  return <RegisterRoombuilding />;
+};
+
+export default building;
